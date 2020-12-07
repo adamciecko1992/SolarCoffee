53a867f3b975c65a75562e13796c8afdaec5aad2
Adam Ciecko <adam.ciecko@hotmail.com>
2020-12-07 01:48:57 +0100

    little improvements to helper method

--- solarcoffee.front/src/helpers/updateValidationState.ts
+++ solarcoffee.front/src/helpers/updateValidationState.ts
@@ -4,7 +4,6 @@ export default function updateValid<T>(
     objectReference: T
 ) {
     (objectReference as any)[property] = value;
-    const validationStateValues = Object.values(objectReference);
 }
 
 
